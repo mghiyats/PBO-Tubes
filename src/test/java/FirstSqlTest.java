285e5f3f2f88afcd5e25963c622ba072d8a8857b
shua24 <jo.daniel.simanjuntak@gmail.com>
2025-06-05 03:28:27 +0700

    Ubah urutan test

--- src/test/java/FirstSqlTest.java
+++ src/test/java/FirstSqlTest.java
@@ -4,13 +4,18 @@ import java.sql.SQLException;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.fail;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 
 import dao.DatabaseConnector;
 
-public class SqlTest {
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class FirstSqlTest {
 
     @Test
+    @Order(1)
     public void testDatabaseConnectionSuccess() {
         try (Connection conn = DatabaseConnector.getConnection()) {
             assertNotNull(conn, "Connection should not be null");
